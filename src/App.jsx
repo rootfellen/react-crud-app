59b5a6fc53e0c61114172807016cd43664da4cf8
serhii <rootfellenua@gmail.com>
2022-07-23 21:40:54 -0400

    adding form to submit info

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    name: "",
+    age: 0,
+    country: "",
+    position: "",
+    wage: 0,
+  });
+
+  function handleChange(e) {
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        name: "",
+        age: 0,
+        country: "",
+        position: "",
+        wage: 0,
+      };
+    });
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} className="information">
+        <label>Name:</label>
+        <input
+          name="name"
+          type="text"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label>Age:</label>
+        <input
+          name="age"
+          type="number"
+          value={formData.age}
+          onChange={handleChange}
+        />
+        <label>Country:</label>
+        <input
+          name="country"
+          type="text"
+          value={formData.country}
+          onChange={handleChange}
+        />
+        <label>Position:</label>
+        <input
+          name="position"
+          type="text"
+          value={formData.position}
+          onChange={handleChange}
+        />
+        <label>Wage (year):</label>
+        <input
+          name="wage"
+          type="number"
+          value={formData.wage}
+          onChange={handleChange}
+        />
+        <button>Add employee</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
